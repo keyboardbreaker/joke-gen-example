fe926a433be892a6de8d66400902180ce20599d9
Yu-Jin Tan <yu-jin@youlend.com>
2021-07-17 00:01:31 +0100

    fetchMyApi function name

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
-
 const useFetch = () => {
   const url = "https://interview-assessment.api.avamae.co.uk/api/v1/home/banner-details";
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect(async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    setData(data.Details);
-    setLoading(false);
-
+  useEffect(() => {
+    async function fetchMyApi()  {
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(data.Details);
+      setLoading(false);
+    }
+    fetchMyApi();
   }, [])
   return { data, loading };
 }
 
-export default () => {
+const App = () => {
   const { data, loading } = useFetch()
-  console.log(data);
   return (
     <section>
       {loading ?
         <div>...loading</div> :
-        <div>{data.map(i=>i.Title)}<br/></div>
+        <div>
+          <ul>
+          {
+            data.map((element, index) => 
+              {
+                return <li key={index}>{index + " " + element.Title}</li>
+              })
+          }
+          </ul>
+        </div>
       }
     </section>
   );
-
 };
+
+export default App;
