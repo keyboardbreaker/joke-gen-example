791501ad8ee8152c1b32c81951e1941431cfc4cf
Yu-Jin Tan <yu-jin@youlend.com>
2021-07-17 00:45:26 +0100

    image slide show solution

--- src/App.js
+++ src/App.js
@@ -19,20 +19,55 @@ const useFetch = () => {
 }
 
 const App = () => {
-  const { data, loading } = useFetch()
+  // initialise
+  const { data, loading } = useFetch();
+  const [current, setCurrent] = useState(0);
+  let length = 0;
+  
+  // waiting for fetch to complete
+  if(data != null && loading === false) {
+    length = data.length;
+  }
+  
+  // slider controls
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+ 
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+ 
+  if (!Array.isArray(data) || length <= 0) {
+    return null;
+  }
+  // END slider controls
+
+  // imgSlides is better name for data if it's just images coming from the fetch, 
+  // so can rename it even earlier, ie, line 6
+  const imgSlides = data;
+
   return (
     <section>
       {loading ?
         <div>...loading</div> :
         <div>
-          <ul>
+          <button onClick={prevSlide}>&lt; left</button>
+          <button onClick={nextSlide}>right &gt;</button>
           {
-            data.map((element, index) => 
-              {
-                return <li key={index}>{index + " " + element.Title}</li>
-              })
+            imgSlides.map((slide, index) => {
+              return (
+                <div
+                  className={index === current ? 'slide active' : 'slide'}
+                  key={index}
+                >
+                  {index === current && (
+                    <img src={slide.ImageUrl} alt={slide.Title} className='image' />
+                  )}
+                </div>
+              );
+            })
           }
-          </ul>
         </div>
       }
     </section>
